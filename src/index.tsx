736af0ea6db6d580536033876303900138cba875
unknown <mazayw@gmail.com>
2023-01-11 21:29:52 +0200

    fix: fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './fonts/comicsansms3-webfont.woff'
-import './fonts/design.graffiti.comicsansmsgras-webfont.woff'
+import './fonts/comic_sans_ms-webfont.ttf'
+import './fonts/comic_sans_ms-webfont.woff'
+import './fonts/comic_sans_ms-webfont.woff2'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

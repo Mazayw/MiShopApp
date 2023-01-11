015004f1889a73033c48b1d0e82c17766e83da5e
unknown <mazayw@gmail.com>
2023-01-11 20:50:18 +0200

    feat: add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/comicsansms3-webfont.ttf'
+import './fonts/design.graffiti.comicsansmsgras-webfont.ttf'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

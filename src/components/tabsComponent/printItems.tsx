a8c9115dba0da5a74327a173497ab398502f8199
unknown <mazayw@gmail.com>
2023-03-21 13:39:51 +0200

    feat: add 100x180mm component

--- src/components/tabsComponent/printItems.tsx
+++ src/components/tabsComponent/printItems.tsx
@@ -1,11 +1,17 @@
-import { ComponentToPrint } from '../ComponentToPrint';
+import { ComponentToPrint100x180 } from '../ComponentToPrint100x180';
+import { ComponentToPrint210x148 } from '../ComponentToPrint210x148';
 
 export const printItems = [
 	{
 		name: '148x210mm',
 		value: 0,
-		component: <ComponentToPrint />,
+		component: <ComponentToPrint210x148 />,
 		src: '/bigPrint.jpg',
 	},
-	{ name: 'new', value: 1, component: null, src: '/favicon.ico' },
+	{
+		name: '100x180mm',
+		value: 1,
+		component: <ComponentToPrint100x180 />,
+		src: '/favicon.ico',
+	},
 ];
